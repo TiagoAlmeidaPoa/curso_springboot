7f96ebe49ad38c1915ee2d9da13f08a96845c003
Tiago Almeida <tiago.almeida.poa@gmail.com>
2021-10-12 23:01:16 -0300

    Projeto criado

---
+++ src/main/java/com/tiagoalmeida/cursinhomc/CursinhomcApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoalmeida.cursinhomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursinhomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursinhomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoalmeida/cursinhomc/CursinhomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoalmeida.cursinhomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursinhomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
