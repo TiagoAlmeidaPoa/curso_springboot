bdcd2a9f43cff78cc92c06935d94c33789a58873
Tiago Almeida <tiago.almeida.poa@gmail.com>
2021-11-02 19:10:38 -0300

    Ajustes no endpoint categorias\{id}

--- src/main/java/com/tiagoalmeida/cursinhomc/services/CategoriaService.java
+++ src/main/java/com/tiagoalmeida/cursinhomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.tiagoalmeida.cursinhomc.entities.Categoria;
 import com.tiagoalmeida.cursinhomc.repositories.CategoriaRepository;
+import com.tiagoalmeida.cursinhomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> {
+			throw new ObjectNotFoundException("Object not found for " + id + ", Tipo: " + Categoria.class.getName());
+		});
 	}
 
 }
